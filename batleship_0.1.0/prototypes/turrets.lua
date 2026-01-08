403368090f2eb81d29ab0cb082937a898ad3d450
Peter Wolf <84736182+peterwolf-pl@users.noreply.github.com>
2026-01-08 02:59:47 +0100

    Lock down batleship artillery turret prototype

--- batleship_0.1.0/prototypes/turrets.lua
+++ batleship_0.1.0/prototypes/turrets.lua
@@ -1,10 +1,11 @@
 local turret = table.deepcopy(data.raw["artillery-turret"]["artillery-turret"])
 
 turret.name = "batleship-artillery-turret"
+turret.localised_name = {"entity-name.batleship-artillery-turret"}
 turret.icon = "__base__/graphics/icons/artillery-turret.png"
 turret.icon_size = 64
-turret.flags = {"placeable-off-grid", "not-on-map"}
-turret.minable = nil
+turret.flags = {"placeable-off-grid", "not-on-map", "not-deconstructable", "not-blueprintable", "not-upgradable"}
+turret.minable = false
 turret.selection_box = {{-0.6, -0.6}, {0.6, 0.6}}
 turret.collision_box = {{-0.4, -0.4}, {0.4, 0.4}}
 turret.selectable_in_game = false
